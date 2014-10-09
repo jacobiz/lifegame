d5d341a3885282c1a21eac4320469ae12fbe820c
yuki_noma <yuki_noma@dwango.co.jp>
2014-10-09 12:30:12 +0900

    コマンドライン引数から設定値を受け取れるように修正

--- src/main/scala/LifeGame.scala
+++ src/main/scala/LifeGame.scala
@@ -1,13 +1,44 @@
-object LifeGame extends App {
-  
-  println("開始")
+object LifeGame {
+  val usage = """
+    Usage example: sbt "run -w <width> -h <height> -g <generation>"
+  """
 
-  var map = CellMap(10, 10)
+  def main(args: Array[String]) {
+    val argList = args.toList
+    type OptionIntMap = Map[Symbol, Int]
 
-  while (map.generation < 10) {
-    Thread.sleep(1000)
+    def nextOption(map: OptionIntMap, list: List[String]): OptionIntMap = {
+      list match {
+        case Nil => map
+        case "-w" :: value :: tail =>
+          nextOption(map ++ Map('width -> value.toInt), tail)
+        case "-h" :: value :: tail =>
+          nextOption(map ++ Map('height -> value.toInt), tail)      
+        case "-g" :: value :: tail =>
+          nextOption(map ++ Map('generation -> value.toInt), tail)
+        case string :: tail => 
+          println(usage)
+          exit(0)
+      }
+    }
+
+    val options = nextOption(
+      Map('width -> 10, 'height -> 10, 'generation -> 5), 
+      argList
+    )
+    val width = options('width)
+    val height = options('height)
+    val generation = options('generation)
+
+    println("Start")
+
+    var map = CellMap(width, height)
     println(map.toString)
-    map = map.nextMap
-  }
 
+    while (map.generation < generation) {
+      Thread.sleep(1000) //miliseconds
+      map = map.nextMap
+      println(map.toString)
+    }
+  }
 }
